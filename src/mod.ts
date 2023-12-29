84fcb1abf2078d2cd4f87bcca4b02dfbc1ee5bfc
Gabriel Vaquer <brielov@icloud.com>
2023-12-29 10:56:55 -0300

    Documented exposed functions

--- src/mod.ts
+++ src/mod.ts
@@ -56,12 +56,22 @@ export interface ObjectSchema<T extends PlainObject> extends Schema<T> {
   shape: Shape<T>;
 }
 
+/**
+ * Creates a schema for validating strings.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for strings.
+ */
 export const str = (message = "Expected string"): Schema<string> => ({
   name: "string",
   parse: (input) =>
     typeof input === "string" ? Ok(input) : fail(message, input),
 });
 
+/**
+ * Creates a schema for validating numbers.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for numbers.
+ */
 export const num = (message = "Expected number"): Schema<number> => ({
   name: "number",
   parse: (input) =>
@@ -70,12 +80,22 @@ export const num = (message = "Expected number"): Schema<number> => ({
       : fail(message, input),
 });
 
+/**
+ * Creates a schema for validating booleans.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for booleans.
+ */
 export const bool = (message = "Expected boolean"): Schema<boolean> => ({
   name: "boolean",
   parse: (input) =>
     typeof input === "boolean" ? Ok(input) : fail(message, input),
 });
 
+/**
+ * Creates a schema for validating dates.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for dates.
+ */
 export const date = (message = "Expected date"): Schema<Date> => ({
   name: "date",
   parse: (input) =>
@@ -84,6 +104,12 @@ export const date = (message = "Expected date"): Schema<Date> => ({
       : fail(message, input),
 });
 
+/**
+ * Creates a schema for validating arrays.
+ * @param schema - The schema to apply to each element of the array.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for arrays.
+ */
 export const vec = <T,>(
   schema: Schema<T>,
   message = "Expected array",
@@ -105,6 +131,12 @@ export const vec = <T,>(
   },
 });
 
+/**
+ * Creates a schema for validating objects with a specific shape.
+ * @param shape - The shape of the object, where each key is associated with a schema.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for objects with a specified shape.
+ */
 export const struct = <T extends PlainObject>(
   shape: Shape<T>,
   message = "Expected object",
@@ -126,16 +158,34 @@ export const struct = <T extends PlainObject>(
   },
 });
 
+/**
+ * Creates a schema for values that can be either of the specified type or null/undefined.
+ * @param schema - The original schema.
+ * @returns A schema for values that can be either of the specified type or null/undefined.
+ */
 export const maybe = <T,>(schema: Schema<T>): Schema<T | null | undefined> => ({
   ...schema,
   parse: (input) => (isNil(input) ? Ok(input) : schema.parse(input)),
 });
 
-export const def = <T,>(schema: Schema<T>, fallback: T): Schema<T> => ({
+/**
+ * Creates a schema with a default value.
+ * If the input is null or undefined, the default value is used.
+ * @param schema - The original schema.
+ * @param fallback - The default value to use when the input is null or undefined.
+ * @returns A new schema with default value handling.
+ */
+export const withDefault = <T,>(schema: Schema<T>, fallback: T): Schema<T> => ({
   ...schema,
   parse: (input) => (isNil(input) ? Ok(fallback) : schema.parse(input)),
 });
 
+/**
+ * Creates a schema that allows values conforming to any of the specified schemas.
+ * @param schemas - An array of schemas.
+ * @param message - Custom error message to be used when none of the schemas match.
+ * @returns A schema that allows values conforming to any of the specified schemas.
+ */
 export const either = <A extends Schema<unknown>, B extends Schema<unknown>[]>(
   schemas: [A, ...B],
   message = `Expected either ${schemas.map((s) => s.name).join(" | ")}`,
@@ -151,6 +201,12 @@ export const either = <A extends Schema<unknown>, B extends Schema<unknown>[]>(
   },
 });
 
+/**
+ * Extends multiple object schemas into a new object schema with a merged shape.
+ * @param schemas - An array of object schemas to extend.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A new object schema with a merged shape.
+ */
 export const extend = <
   A extends ObjectSchema<PlainObject>,
   B extends ObjectSchema<PlainObject>[],
@@ -162,6 +218,12 @@ export const extend = <
   return struct(newShape, message);
 };
 
+/**
+ * Creates a schema for validating arrays with a specified tuple structure.
+ * @param schemas - An array of schemas, each corresponding to an element in the tuple.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating arrays with a specified tuple structure.
+ */
 export const tuple = <A extends Schema<unknown>, B extends Schema<unknown>[]>(
   schemas: [A, ...B],
   message = `Expecting array`,
@@ -183,6 +245,12 @@ export const tuple = <A extends Schema<unknown>, B extends Schema<unknown>[]>(
   },
 });
 
+/**
+ * Creates a schema for validating objects with a subset of keys.
+ * @param schema - The original object schema.
+ * @param keys - An array of keys to include in the subset.
+ * @returns A schema for objects with a subset of keys.
+ */
 export const pick = <T extends PlainObject, K extends keyof T>(
   schema: ObjectSchema<T>,
   keys: readonly K[],
@@ -196,6 +264,12 @@ export const pick = <T extends PlainObject, K extends keyof T>(
   return struct(newShape);
 };
 
+/**
+ * Creates a schema for validating objects with certain keys omitted.
+ * @param schema - The original object schema.
+ * @param keys - An array of keys to omit from the object.
+ * @returns A schema for objects with specified keys omitted.
+ */
 export const omit = <T extends PlainObject, K extends keyof T>(
   schema: ObjectSchema<T>,
   keys: readonly K[],
@@ -209,6 +283,12 @@ export const omit = <T extends PlainObject, K extends keyof T>(
   return struct(newShape);
 };
 
+/**
+ * Creates a schema for transforming values using a provided function.
+ * @param schema - The original schema.
+ * @param f - The function to transform the value.
+ * @returns A schema for transformed values.
+ */
 export const transform = <T, U>(
   schema: Schema<T>,
   f: (value: T) => U,
@@ -221,6 +301,13 @@ export const transform = <T, U>(
   },
 });
 
+/**
+ * Creates a schema for validating values based on a custom condition.
+ * @param schema - The original schema.
+ * @param f - The condition function. Should return true if the value is valid.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for values meeting a custom condition.
+ */
 export const refine = <T,>(
   schema: Schema<T>,
   f: (value: T) => boolean,
@@ -236,6 +323,13 @@ export const refine = <T,>(
   },
 });
 
+/**
+ * Creates a schema for coercing values to a specified type.
+ * Supports coercion to string, number, boolean, and Date types.
+ * @param schema - The original schema.
+ * @returns A schema for values coerced to the specified type.
+ * @throws {Error} If the specified schema type cannot be coerced.
+ */
 export const coerce = <T extends string | number | boolean | Date>(
   schema: Schema<T>,
 ): Schema<T> => {
@@ -273,6 +367,13 @@ export const coerce = <T extends string | number | boolean | Date>(
   };
 };
 
+/**
+ * Parses an unknown input using the specified schema, throwing an error if the validation fails.
+ * @param schema - The schema to use for parsing.
+ * @param input - The input to parse.
+ * @returns The parsed value.
+ * @throws {Error} If the input does not match the schema.
+ */
 export const unsafeParse = <T,>(schema: Schema<T>, input: unknown): T => {
   const result = schema.parse(input);
   if (!result.ok) {
@@ -283,26 +384,71 @@ export const unsafeParse = <T,>(schema: Schema<T>, input: unknown): T => {
 
 const EMAIL_REGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
+/**
+ * Creates a schema for validating email addresses.
+ * @param schema - The original schema.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating email addresses.
+ */
 export const email = <T extends string>(
   schema: Schema<T>,
   message = `Expected valid email address`,
 ) => refine(schema, (s) => EMAIL_REGEX.test(s), message);
 
+const UUID_REGEX =
+  /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
+
+/**
+ * Validates that a string is a valid UUID.
+ * @param schema - The original schema for a string.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A new schema for UUID validation.
+ */
+export const uuid = <T extends string>(
+  schema: Schema<T>,
+  message = "Expected a valid UUID",
+): Schema<T> => refine(schema, (s) => UUID_REGEX.test(s), message);
+
+/**
+ * Creates a schema for validating that a number is an integer.
+ * @param schema - The original schema.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating integers.
+ */
 export const int = <T extends number>(
   schema: Schema<T>,
   message = "Expecting number to be an integer",
 ) => refine(schema, (x) => Number.isInteger(x), message);
 
+/**
+ * Creates a schema for validating that a number is positive.
+ * @param schema - The original schema.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating positive numbers.
+ */
 export const positive = <T extends number>(
   schema: Schema<T>,
   message = "Expected number to be positive",
 ) => refine(schema, (x) => x > 0, message);
 
+/**
+ * Creates a schema for validating that a number is negative.
+ * @param schema - The original schema.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating negative numbers.
+ */
 export const negative = <T extends number>(
   schema: Schema<T>,
   message = "Expected number to be negative",
 ) => refine(schema, (x) => x < 0, message);
 
+/**
+ * Creates a schema for clamping a number within a specified range.
+ * @param schema - The original schema.
+ * @param min - The minimum value of the range.
+ * @param max - The maximum value of the range.
+ * @returns A schema for clamped numbers.
+ */
 export const clamp = <T extends number>(
   schema: Schema<T>,
   min: number,
@@ -315,21 +461,52 @@ const toComparableInt = (input: number | string | Date) => {
   return input.getTime();
 };
 
+/**
+ * Creates a schema for validating that a numeric value is greater than or equal to a specified minimum.
+ * @param schema - The original schema.
+ * @param value - The minimum value to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating minimum values.
+ */
 export function min(
   schema: Schema<number>,
   value: number,
   message?: string,
 ): Schema<number>;
+
+/**
+ * Creates a schema for validating that a string length or date is greater than or equal to a specified minimum.
+ * @param schema - The original schema.
+ * @param value - The minimum value to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating minimum values.
+ */
 export function min(
   schema: Schema<string>,
   value: number,
   message?: string,
 ): Schema<string>;
+
+/**
+ * Creates a schema for validating that a date is greater than or equal to a specified minimum date.
+ * @param schema - The original schema.
+ * @param value - The minimum date value to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating minimum date values.
+ */
 export function min(
   schema: Schema<Date>,
   value: Date,
   message?: string,
 ): Schema<Date>;
+
+/**
+ * Creates a schema for validating that a value is greater than or equal to a specified minimum.
+ * @param schema - The original schema.
+ * @param value - The minimum value or date to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating minimum values.
+ */
 export function min(
   // deno-lint-ignore no-explicit-any
   schema: Schema<any>,
@@ -343,21 +520,52 @@ export function min(
   );
 }
 
+/**
+ * Creates a schema for validating that a numeric value is less than or equal to a specified maximum.
+ * @param schema - The original schema.
+ * @param value - The maximum value to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating maximum values.
+ */
 export function max(
   schema: Schema<number>,
   value: number,
   message?: string,
 ): Schema<number>;
+
+/**
+ * Creates a schema for validating that a string length or date is less than or equal to a specified maximum.
+ * @param schema - The original schema.
+ * @param value - The maximum value to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating maximum values.
+ */
 export function max(
   schema: Schema<string>,
   value: number,
   message?: string,
 ): Schema<string>;
+
+/**
+ * Creates a schema for validating that a date is less than or equal to a specified maximum date.
+ * @param schema - The original schema.
+ * @param value - The maximum date value to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating maximum date values.
+ */
 export function max(
   schema: Schema<Date>,
   value: Date,
   message?: string,
 ): Schema<Date>;
+
+/**
+ * Creates a schema for validating that a value is less than or equal to a specified maximum.
+ * @param schema - The original schema.
+ * @param value - The maximum value or date to compare against.
+ * @param message - Custom error message to be used when the validation fails.
+ * @returns A schema for validating maximum values.
+ */
 export function max(
   // deno-lint-ignore no-explicit-any
   schema: Schema<any>,
@@ -370,3 +578,12 @@ export function max(
     message,
   );
 }
+
+/**
+ * Creates a schema that accepts any value and marks it as unknown.
+ * @returns A schema for any unknown value.
+ */
+export const unknown = (): Schema<unknown> => ({
+  name: "unknown",
+  parse: Ok,
+});
